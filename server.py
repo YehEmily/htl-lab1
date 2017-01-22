ed25cb78cbba9dfc565131f0d822ea249b8a9b59
YehEmily <emilyyeh1997@gmail.com>
2017-01-21 19:25:48 -0500

    Server Python file

--- server.py
+++ server.py
@@ -25,7 +25,18 @@ def home_page():
 
 @app.route('/area/<course_area>')
 def area_page(course_area):
-    return render_template('course_area.html', courses=courses[courses.course_area == course_area].iterrows())
+    return render_template('course_area.html', area=course_area, courses=courses[courses.course_area == course_area].iterrows())
+
+@app.route('/instructor/<instructor_name>')
+def instructor_page(instructor_name):
+	return render_template('instructor_pages.html', name=instructor_name, instructors=courses[courses.course_contact == instructor_name].iterrows())
+
+@app.route('/courses/<course_number>')
+def course_page(course_number):
+	return render_template('course_pages.html', current_course=courses[courses.course_number == course_number].iterrows())
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# Add area descriptions to the Course Area pages.
+# Bring these in from a CSV or other data file, and get the values to the template file.
